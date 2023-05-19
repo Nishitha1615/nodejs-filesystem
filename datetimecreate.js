adb6b6fb462e4beee86a56c26af2adb51fa65bdd
Nishitha1615 <nishitham0@gmail.com>
2023-05-19 22:43:31 +0530

    re-submission

---
+++ datetimecreate.js
@@ -0,0 +1,32 @@
+import express from "express";
+import fs from 'fs'
+
+const app = express()
+const dateObject = new Date();
+const date = (`0 ${dateObject.getDate()}`).slice(-2);
+const month = (`0 ${dateObject.getMonth() + 1}`).slice(-2);
+ const year = dateObject.getFullYear();
+ // current hours
+const hours = dateObject.getHours();
+// current minutes
+const minutes = dateObject.getMinutes();
+// current seconds
+const seconds = dateObject.getSeconds();
+ const text=`${year}-${month}-${date} ${hours}:${minutes}:${seconds}`
+
+// const dir= './Nodetask/';
+//Q1) Write API endpoint which will create a text file in a particular folder.
+app.get('/', function (req, res) {
+  res.send(`<h1>Created the datetime -${text}file under folder name-text</h1>`)
+//  res.send(   fs.writeFile(`./${text}.txt`, text, (err)=> {console.log( `Completed file${text}`)}))
+    res.send(fs.writeFile(`./text/${date}-${month}-${year}-${hours}.txt`,text,(err)=>
+    {
+        console.log(`completed date-time-creation file-${text} `)
+    }))
+  })
+  app.listen(5000) 
+
+
+
+
+
